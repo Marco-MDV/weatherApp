07b21d11b4ece6b719518a4302ba9281f252e45a
Marco-MDV <marcodevincentiis71@gmail.com>
2024-12-08 10:46:29 +0100

    Fix HoursPanel content

--- weather-app/src/components/weatherPanels/mainPanel/hoursPanel/HoursPanel.jsx
+++ weather-app/src/components/weatherPanels/mainPanel/hoursPanel/HoursPanel.jsx
@@ -8,7 +8,10 @@ export default function HoursPanel({ hourlyData }) {
           return <div key={i} className='text-center hover:font-semibold min-w-[60px] text-sky-600 hover:text-white'>
             <p>{new Date(e.date).getHours()}:00</p>
             <p>&#127777; {e.temperature}°</p>
-            <p>&#9748; {e.precipitation.total}%</p>
+            <div>
+              <p>&#9748;</p>
+              <p>{e.precipitation.total}%</p>
+            </div>
           </div> 
         })
       }
